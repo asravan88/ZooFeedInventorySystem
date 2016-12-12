7b188c56ce11d0daa86eed6ca400880cceff3761
aily <aily@LAPTOP-QHUDND5C>
2016-12-12 00:05:17 -0700

    Methods generating -- sixth commit

---
+++ src/com/zfis/manager/InventoryRecords.java
@@ -0,0 +1,16 @@
+package com.zfis.manager;
+
+import com.zfis.pojo.Zoo;
+
+public class InventoryRecords {
+	
+	public int avgFeedPerDayByAnimalByZoo(Zoo zoo){
+		return 100/2;
+	}
+	
+	
+	public int NumOfFeedsPerAnimalByZoo(Zoo zoo){
+		return 0;
+	}
+	
+}
